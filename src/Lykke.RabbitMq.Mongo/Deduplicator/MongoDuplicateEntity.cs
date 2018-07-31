5fa735461fb7013fa1774b8c7cd56c828b5f1295
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-07-31 16:28:16 +0300

    added project, added mongo deduplicator

---
+++ src/Lykke.RabbitMq.Mongo/Deduplicator/MongoDuplicateEntity.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Lykke.RabbitMq.Mongo.Deduplicator
+{
+    internal class MongoDuplicateEntity
+    {
+        [BsonId]
+        public string BsonId { get; set; }
+
+    }
+}
